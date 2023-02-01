9bacc66db33155928f59af85e754ec72afa4ca62
flyworkspace <flyworkspace@gmail.com>
2023-02-01 16:36:57 +0800

    aaa

--- app/src/main/java/com/flyworkspace/androiddemo/MainActivity.kt
+++ app/src/main/java/com/flyworkspace/androiddemo/MainActivity.kt
@@ -2,10 +2,13 @@ package com.flyworkspace.androiddemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val a = Dispatchers.Main + Job()
     }
 }
